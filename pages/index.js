437385a884c92f12fd8048a92d125aed0ea8f475
Rajiv Boon <vrajivboon@gmail.com>
2019-05-12 22:36:12 +0530

    9. Shared Components-Header

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,13 @@
 import React from 'react';
-import Link from 'next/link';
+import Header from '../components/shared/Header';
 
 class Index extends React.Component {
     render() {
     return ( 
         <div>
+            <Header />
             <h1> This is Index page</h1>
-            <Link href="/">
-            <a >Home</a>
-            </Link> 
-                
-            <Link href="/about">
-            <a>About</a>
-            </Link>
-
-            <Link href="/blogs">
-            <a >Blogs</a>
-            </Link>
-
-            <Link href="/portfolios">
-            <a >Portfolios</a>
-            </Link>
-
-            <Link href="/cv">
-            <a>Cv</a>
-            </Link>
-
-
-
+            
         </div>
 
         )
