97e51a73c10ef913abcf88262203fbe1b1bd6e4b
Rajiv Boon <vrajivboon@gmail.com>
2019-05-13 02:27:11 +0530

    13. Lifecycle Function

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,42 @@ import Header from '../components/shared/Header';
 import BaseLayout from '../components/layouts/BaseLayout';
 
 class Index extends React.Component {
+
+    constructor() {
+        super();
+
+        this.state = {
+            titiel: 'i am index Page'
+        }
+        console.log('constructor');
+    }
+
+    componentDidMount() {
+        console.log('componentDidMount');
+    }
+
+    ComponentDidUpdate() {
+        console.log('shouldComponentUpdate');
+    }
+
+    componentWillUnmount() {
+        console.log('componentWillUnmount');
+    }
+
+    updateTitle() {
+        this.setState({ titiel: 'i am updated' })
+     
+    }
+
+
+
     render() {
+        console.log('render');
     return ( 
         <BaseLayout>
             <h1> This is Index page</h1>
+            <h2> {this.state.titiel}  </h2>
+            <button onClick={() => { this.updateTitle() }}>change titiel</button>
         </BaseLayout> 
             
         
