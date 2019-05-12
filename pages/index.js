71be6f33a7a92889bfd4622e99e6ac0bec87ede1
Rajiv Boon <vrajivboon@gmail.com>
2019-05-12 22:24:59 +0530

    8.SPA vs MPA

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,33 @@
 import React from 'react';
+import Link from 'next/link';
 
 class Index extends React.Component {
     render() {
     return ( 
         <div>
             <h1> This is Index page</h1>
-            <a href="/">Home</a>
-            <a href="/about">About</a>
-            <a href="/blogs">Blogs</a>
-            <a href="/portfolios">Portfolios</a>
-            <a href="/cv">Cv</a>
+            <Link href="/">
+            <a >Home</a>
+            </Link> 
+                
+            <Link href="/about">
+            <a>About</a>
+            </Link>
+
+            <Link href="/blogs">
+            <a >Blogs</a>
+            </Link>
+
+            <Link href="/portfolios">
+            <a >Portfolios</a>
+            </Link>
+
+            <Link href="/cv">
+            <a>Cv</a>
+            </Link>
+
+
+
         </div>
 
         )
