f9baaa189c166d5dd4bd8240e3072cdbaeb0bf23
Rajiv Boon <vrajivboon@gmail.com>
2019-05-13 07:45:00 +0530

    25. Dynamic Route Fix partA

--- server.js
+++ server.js
@@ -10,6 +10,12 @@ app
     .then(() => {
         const server = express()
 
+        server.get('/portfolio/:id', (req, res) => {
+            const actualPage = '/portfolio'
+            const queryParams = { title: req.params.id }
+            app.render(req, res, actualPage, queryParams)
+        })
+
         server.get('*', (req, res) => {
             return handle(req, res)
         })
