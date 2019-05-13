914e23470f63d2cb3e789e2b8172975212ca14af
Rajiv Boon <vrajivboon@gmail.com>
2019-05-13 06:40:59 +0530

    21. Fetch Posts from API

--- pages/index.js
+++ pages/index.js
@@ -6,25 +6,7 @@ import axios from 'axios';
 
 class Index extends SuperComponet {
 
-    static async getInitialProps() {
-        let userData = {};
-            
-        try {
-            const response = await axios.get("https://jsonplaceholder.typicode.com/todos/1");
-            userData =response.data;
-        } catch (error) {
-            console.error(error);
-        }
-        
-
-        // axios.get("https://jsonplaceholder.typicode.com/todos/1")
-        //     .then(
-        //         (response) => console.log(response.data)
-        //     )
-        //     .catch(err => console.error(err))
-        
-        return { initialData: [1, 2, 3, 4], userData };
-    }
+   
 
     constructor(props) {
         super(props);
@@ -59,13 +41,11 @@ class Index extends SuperComponet {
     render() {
         
         const title = this.state.titiel;
-        const { userData, initialData } = this.props;
-        // const initialData = this.props.initialData;
+        
     return ( 
         <BaseLayout>
             <h1> This is Index page</h1>
-            <h2> {title}  </h2>
-            <h2>{userData.title}</h2>
+            <h2> {title}  </h2>            
             <button onClick={() => { this.updateTitle() }}>change titiel</button>
         </BaseLayout> 
             
--- pages/portfolios.js
+++ pages/portfolios.js
@@ -1,11 +1,45 @@
 import React from 'react';
 import BaseLayout from '../components/layouts/BaseLayout';
+import axios from 'axios';
+
 
 class Portfolios extends React.Component {
+
+    static async getInitialProps() {
+        let posts= [];
+
+        try {
+            const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+            posts = response.data;
+        } catch (error) {
+            console.error(error);
+        }       
+        return { posts: posts.splice(0, 10)};
+    }
+    
+    renderPosts(posts) {
+        return posts.map((post) => {
+            return (
+                <li>
+                    {post.title}
+                </li>
+            )
+        })
+    }
+
     render() {
+        const { posts } = this.props;
         return (
             <BaseLayout>
             <h1> This is Portfolios page</h1>
+                <ul>
+                    {this.renderPosts(posts)}
+                    {/* {posts.map((post) => {
+                        return (
+                            <li>{post.title} </li>
+                        )
+                    })} */}
+            </ul>
             </BaseLayout>
         )
     }
