c7abf2d04077ca8441c13aba9e2807736659e5ef
Rajiv Boon <vrajivboon@gmail.com>
2019-05-14 13:30:22 +0530

    45. Save Tokens in Cookies

--- services/auth0.js
+++ services/auth0.js
@@ -1,4 +1,5 @@
 import auth0 from 'auth0-js';
+import Cookies from 'js-cookie';
 
 class Auth {
     
@@ -14,12 +15,14 @@ class Auth {
     });
         
         this.login = this.login.bind(this);
+        this.logout = this.logout.bind(this);
         this.handleAuthentication = this.handleAuthentication.bind(this);
+        this.isAuthenticated = this.isAuthenticated.bind(this);
     
 }
 
     handleAuthentication() {
-        debugger;
+        
         return new Promise((resolve, reject) => {
             this.auth0.parseHash((err, authResult) => {
                 if (authResult && authResult.accessToken && authResult.idToken) {
@@ -36,6 +39,35 @@ class Auth {
     }
 
     setSession(authResult) {
+       
+        debugger;
+        // Set the time that the Access Token will expire at
+        const expiresAt = (authResult.expiresIn * 1000) + new Date().getTime();
+        // this.accessToken = authResult.accessToken;
+        // this.idToken = authResult.idToken;
+        // this.expiresAt = expiresAt;
+           
+       
+        Cookies.set('user', authResult.idTokenPayload);
+        Cookies.set('jwt', authResult.idToken);
+        Cookies.set('expiresAt', expiresAt);
+    }
+
+    logout(){
+            Cookies.remove('user');
+            Cookies.remove('jwt');
+            Cookies.remove('expiresAt');
+            this.auth0.logout({
+              returnTo:'',
+              clientID:'0ijPBeMRqJHNBxvQoQ9BeI7tde5qLqnQ'
+            })
+    }
+    
+    isAuthenticated() {
+        // Check whether the current time is past the
+        // access token's expiry time
+        const expiresAt = Cookies.getJSON('expiresAt');
+        return new Date().getTime() < expiresAt;
     }
 
     login() {
