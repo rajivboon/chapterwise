1492de0c6b06c1e1a6d1d49e33cac98cddfad120
Rajiv Boon <vrajivboon@gmail.com>
2019-05-14 12:46:23 +0530

    43. Callback Page and Redirect User

--- services/auth0.js
+++ services/auth0.js
@@ -10,7 +10,8 @@ class Auth {
         redirectUri: 'http://localhost:3000/callback',
         responseType: 'token id_token',
         scope: 'openid profile'
-       });
+       
+    });
         
         this.login = this.login.bind(this);
         this.handleAuthentication = this.handleAuthentication.bind(this);
@@ -18,15 +19,20 @@ class Auth {
 }
 
     handleAuthentication() {
-        this.auth0.parseHash((err, authResult) => {
-            if (authResult && authResult.accessToken && authResult.idToken) {
-                this.setSession(authResult);
-            } else if (err) {
-                // history.replace('/home');
-                console.log(err);
-                // alert(`Error: ${err.error}. Check the console for further details.`);
-            }
-        });
+        debugger;
+        return new Promise((resolve, reject) => {
+            this.auth0.parseHash((err, authResult) => {
+                if (authResult && authResult.accessToken && authResult.idToken) {
+                    this.setSession(authResult);
+                    resolve();
+                } else if (err) {
+                    reject(err)
+                    // history.replace('/home');
+                    console.log(err);
+                    // alert(`Error: ${err.error}. Check the console for further details.`);
+                }
+            });
+        })
     }
 
     setSession(authResult) {
