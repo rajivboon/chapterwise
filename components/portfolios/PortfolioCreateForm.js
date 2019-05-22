6600caa6cb1b3a4e9d543c6ef92e9e9a2ab4a964
Rajiv Boon <vrajivboon@gmail.com>
2019-05-22 06:21:49 +0530

    70. More Input Types part 2

--- components/portfolios/PortfolioCreateForm.js
+++ components/portfolios/PortfolioCreateForm.js
@@ -6,24 +6,16 @@ export default class PortfolioCreateForm extends React.Component {
         super(props);
         this.state = { title: '', description: '', language: '' };  //Value:
 
-        this.handleChange = this.handleChange.bind(this);
-        this.handleChangeDiscription = this.handleChange.bind(this);
-        this.handleChangeLanguage = this.handleChange.bind(this);
+        this.handleChange = this.handleChange.bind(this);       
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleChange(event) {       
-        this.setState({ title: event.target.value });
+        // debugger;
+        const field = event.target.name;
+        this.setState({ [field]: event.target.value });
     }
-
-    handleChangeDiscription(event) {
-        this.setState({ discription: event.target.value });
-    }
-
-    handleChangeLanguage(event) {
-        this.setState({ language: event.target.value });
-    }
-
+   
     handleSubmit(event) {
         alert('A name was submitted: ' + this.state.title + ' ' + this.state.description + ' ' + this.state.language );
         event.preventDefault();
@@ -34,15 +26,15 @@ export default class PortfolioCreateForm extends React.Component {
             <form onSubmit={this.handleSubmit}>
                 <label>
                     Name:
-          <input type="text" value={this.state.value} onChange={this.handleChange} />
+          <input name="title" type="text" value={this.state.value} onChange={this.handleChange} />
                 </label>
                 <label>
                     Discription:
-                  <textarea value={this.state.discription} onChange={this.handleChangeDiscription} />
+                  <textarea name="description" value={this.state.description} onChange={this.handleChange} />
                 </label>
                 <label>
                     Pick your favorite Language:
-          <select value={this.state.language} onChange={this.handleChangeLanguage}>
+          <select name="language" value={this.state.language} onChange={this.handleChange}>
                         <option value="javascript">Javascript</option>
                         <option value="java">Java</option>
                         <option value="c++">C++</option>
