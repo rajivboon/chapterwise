1d801790f2c381af3f421f0ff386465659625580
Rajiv Boon <vrajivboon@gmail.com>
2019-05-22 18:48:11 +0530

    77. Style Date Picker

--- components/form/PortDate.js
+++ components/form/PortDate.js
@@ -1,6 +1,8 @@
 import React from "react";
 import DatePicker from "react-datepicker";
+import { FormGroup, Label } from 'reactstrap';
 import "react-datepicker/dist/react-datepicker.css";
+import moment from 'moment';
 
 export default class PortDate extends React.Component {
     constructor(props) {
@@ -26,14 +28,26 @@ export default class PortDate extends React.Component {
 
     render() {
         const { isBrowserLoaded } = this.state;
+        const { label } = this.props;
 
         return (
             <React.Fragment>
+                <FormGroup>
+                    <Label>{label} </Label>
+                    <div className="input-group">
+
                 {isBrowserLoaded && <DatePicker
                     selected={this.state.dateValue}
-                    onChange={this.handleChange}
+                            onChange={this.handleChange}
+                            peekNextMonth
+                            showMonthDropdown
+                            scrollableYearDropdown
+                            maxDate={moment()}
+                            dropdownMode="select"
                 />
-                }
+            }
+            </div>
+        </FormGroup>
             </React.Fragment>
         );
     }
