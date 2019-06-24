3684523a703b3f852583033e0463d53af91f35d9
Rajiv Boon <vrajivboon@gmail.com>
2019-06-24 10:39:33 +0530

    87 Books Routes and Controllers

--- server/index.js
+++ server/index.js
@@ -14,6 +14,8 @@ const config = require('./config');
 const Book = require('./models/book');
 const bodyParser = require('body-parser');
 
+const bookRoutes = require('./routes/book');
+
 const secretData = [
     {
         title: 'seredafd data 1',
@@ -34,64 +36,9 @@ mongoose.connect(config.DB_URI, { useNewUrlParser: true })
 app.prepare()
     .then(() => {
         const server = express();
-
         server.use(bodyParser.json());
 
-        server.post('/api/v1/books', (req, res) => {
-            const bookData = req.body;
-            console.log(bookData);
-
-            const book = new Book(bookData);
-
-            book.save((err, createdBook) => {
-                if (err) {
-                    return res.status(422).send(err);
-                }
-                return res.json(createdBook);
-            });
-        });
-
-        server.get('/api/v1/books', (req, res) => {
-            Book.find({}, (err, allBooks) => {
-                if (err) {
-                    return res.status(422).send(err);
-                }
-                return res.json(allBooks);
-            })
-        });
-
-        server.patch('/api/v1/books/:id', (req, res) => {
-            const bookId = req.params.id;
-            const bookData = req.body;
-             
-            Book.findById(bookId, (err, foundBook) => {
-                if (err) {
-                    return res.status(422).send(err);
-                }
-                foundBook.set(bookData);
-                foundBook.save((err, savedBook) => {
-                    if (err) {
-                        return res.status(422).send(err);
-                    }
-                    return res.json(foundBook);
-                });                
-            })
-        })
-
-        server.delete('/api/v1/books/:id', (req, res) => {
-            const bookId = req.params.id;
-
-            Book.deleteOne({ _id: bookId }, (err, deletedBook) => {
-                if (err) {
-                    return res.status(422).send(err);
-                }
-
-                return res.json({ status: 'Delited' });
-            })
-        });
-
-
-
+        server.use('/api/v1/books', bookRoutes);
 
         server.get('/api/v1/secret', authService.checkJWT, (req, res) => {
             console.log('-------------consoling User----------------');
