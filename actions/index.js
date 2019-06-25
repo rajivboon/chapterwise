333bbb9a6a2ba4991251c88afd04d8897dfebf76
Rajiv Boon <vrajivboon@gmail.com>
2019-06-25 10:18:56 +0530

    91. Get Portfolios on Client Side

--- actions/index.js
+++ actions/index.js
@@ -20,12 +20,6 @@ export const getSecretData = async (req) => {
     return await axios.get( url, setAuthHeader(req) ).then(response => response.data ) ;
 }
 
-// export const getSecretDataServer = async (req) => {
-//     return await axios.get('http://localhost:3000/api/v1/secret', setAuthHeader(req)).then(response => response.data);
-    
-// }
-
-
-// 1. return await axios.get('/api/v1/secret').then((response) => { return response.data });
-// 2. return await axios.get('/api/v1/secret', { headers: { 'authorization': 'Testing header' } }).then(response => response.data);
-// 3. return await axios.get('/api/v1/secret', { headers: {'authorization': `Bearer ${Cookies.getJSON('jwt')} `}} ).then(response => response.data ) ;
+export const getPortfolios = async (req) => {
+    return await axios.get('http://localhost:3000/api/v1/portfolios', setAuthHeader(req)).then(response => response.data);
+}
--- pages/portfolios.js
+++ pages/portfolios.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import BaseLayout from '../components/layouts/BaseLayout';
-import axios from 'axios';
-// import Link from 'next/link';
+import { getPortfolios } from '../actions';
 import { Link } from '../routes'
 import BasePage from '../components/Basepage';
 import {
@@ -12,30 +11,32 @@ import {
 
 class Portfolios extends React.Component {
 
-    static async getInitialProps() {
-        let posts= [];
+    static async getInitialProps({req}) {
+       
+
+        let portfolios = [];
 
         try {
-            const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
-            posts = response.data;
-        } catch (error) {
-            console.error(error);
-        }       
-        return { posts: posts.splice(0, 10)};
+            portfolios = await getPortfolios(req);
+        } catch (err) {
+            console.error(err);
+        }
+        return { portfolios };
+
     }
     
-    renderPosts(posts) {
-        return posts.map((post, index) => {
+    renderPortfolios(portfolios) {
+        return portfolios.map((portfolio, index) => {
             return (
-                <Col md="4">
-                    <React.Fragment key={index}>
+                <Col key={index} md="4">
+                    <React.Fragment >
                         <span>
                             <Card className="portfolio-card">
-                                <CardHeader className="portfolio-card-header">Some Position {index}</CardHeader>
+                                <CardHeader className="portfolio-card-header">{portfolio.position} </CardHeader>
                                 <CardBody>
-                                    <p className="portfolio-card-city"> Some Location {index} </p>
-                                    <CardTitle className="portfolio-card-title">Some Company {index}</CardTitle>
-                                    <CardText className="portfolio-card-text">Some Description {index}</CardText>
+                                    <p className="portfolio-card-city"> {portfolio.location} </p>
+                                    <CardTitle className="portfolio-card-title">{portfolio.title} </CardTitle>
+                                    <CardText className="portfolio-card-text"> {portfolio.description} </CardText>
                                     <div className="readMore"> </div>
                                 </CardBody>
                             </Card>
@@ -48,12 +49,12 @@ class Portfolios extends React.Component {
     }
 
     render() {
-        const { posts } = this.props;
+        const { portfolios } = this.props;
         return (
             <BaseLayout {...this.props.auth}>
                 <BasePage className="portfolio-page" title="Portfolios">
                 <Row>
-                    {this.renderPosts(posts)}
+                    {this.renderPortfolios(portfolios)}
                     {/* {posts.map((post) => {
                         return (
                             <li>{post.title} </li>
