42631b857b057b0f3fd568af70aa522880f79139
Rajiv Boon <vrajivboon@gmail.com>
2019-06-25 09:32:58 +0530

    90. Portfolio Update and Delete Endpoint

--- server/routes/portfolio.js
+++ server/routes/portfolio.js
@@ -14,7 +14,14 @@ router.post('', authService.checkJWT,
 router.get('', authService.checkJWT,
                 authService.checkRole('siteOwner'),
                 portfolioCtrl.getPortfolios);
+                
+router.patch('/:id', authService.checkJWT,
+    authService.checkRole('siteOwner'),
+    portfolioCtrl.updatePortfolio);
 
+router.delete('/:id', authService.checkJWT,
+    authService.checkRole('siteOwner'),
+    portfolioCtrl.deletePortfolio);
 
 
 module.exports = router;
