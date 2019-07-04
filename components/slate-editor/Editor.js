cc46f40aea52e317c686edf2736067bade940583
Rajiv Boon <vrajivboon@gmail.com>
2019-07-05 00:10:17 +0530

    109 Event Handler in Slate

--- components/slate-editor/Editor.js
+++ components/slate-editor/Editor.js
@@ -36,8 +36,27 @@ export default class SlateEditor extends React.Component {
         this.setState({ value })
     }
 
+    // Define a new handler which prints the key that was pressed.
+    onKeyDown = (event, editor, next) => {
+        // Return with no changes if the keypress is not '&'
+        if (event.key !== '&') return next()
+
+        // Prevent the ampersand character from being inserted.
+        event.preventDefault()
+
+        // Change the value by inserting 'and' at the cursor's position.
+        editor.insertText('and')
+    } 
     // Render the editor.
     render() {
-        return <Editor value={this.state.value} onChange={this.onChange} />
+        return <Editor value={this.state.value}
+        onChange={this.onChange}
+            onKeyDown={this.onKeyDown}
+            />
+        }
     }
-}
+                                // onKeyDown = (event, editor, next) => {
+                                //     console.log(event.key)
+                                //     // debugger;
+                                //     return next()
+                                // }
